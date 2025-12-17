93e12b23057b1c50d9c21ccc44deee7c779e9ce8
Mark Servilla <mark.servilla@gmail.com>
2025-12-16 18:15:05 -0700

    Summary: Add Pytest configuration file stub.

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Pytest configuration container in support of their implicit is better than explicit.
+
+Module:
+    conftest
+
+Author:
+    servilla
+
+Created:
+    2025-12-16
+"""
